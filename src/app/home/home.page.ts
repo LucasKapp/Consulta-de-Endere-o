81fe355de484edb3a8196062177db2663a250223
Lucas Kapp <lucaskapp40@hotmail.com>
2025-05-29 22:52:52 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: false,
+})
+export class HomePage implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+  }
+}
